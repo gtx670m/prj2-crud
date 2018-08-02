e020f79ee64d29a1abe4947ed5f932db144f786a
Quang Le <quang.le@infonam.com>
2018-08-02 17:37:02 +0700

    done

--- src/App.js
+++ src/App.js
@@ -1,79 +1,227 @@
 import React, { Component } from 'react';
 import './App.css';
+import TaskForm from './components/TaskForm';
+import Control from './components/Control';
+import TaskList from './components/TaskList';
+var randomString = require('random-string');
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tasks: [], //id: unique, name, status
+      isDisplayForm: false,
+      editingTaskContent: null,
+      filter: {
+        name: '',
+        status: -1
+      },
+      keyword: '',
+      sortBy: 'name',
+      sortValue: 1
+    }
+  }
+  componentWillMount() {
+    if (localStorage && localStorage.getItem('tasks')) {
+      var tasks = JSON.parse(localStorage.getItem('tasks'));
+      this.setState({
+        tasks: tasks
+      });
+    }
+  }
+  toggleTaskForm = () => {
+    if (this.state.isDisplayForm && this.state.editingTaskContent) {
+      this.setState({
+        isDisplayForm: true,
+        editingTaskContent: null
+      });
+    } else {
+      this.setState({
+        isDisplayForm: !this.state.isDisplayForm,
+        editingTaskContent: null
+      });
+    }
+
+  }
+  closeTaskForm = () => {
+    this.setState({
+      isDisplayForm: false
+    });
+  }
+  openTaskForm = () => {
+    this.setState({
+      isDisplayForm: true
+    });
+  }
+  onSubmit = (data) => {
+    var { tasks } = this.state;
+    if (data.id === '') {
+      var newData = {
+        id: randomString(),
+        name: data.name,
+        status: data.status === 'false' ? false : true
+      }
+      tasks.push(newData);
+    } else {
+      //Editing
+      var index = this.findIndex(data.id);
+      tasks[index] = data
+    }
+    this.setState({
+      tasks: tasks,
+      editingTaskContent: null
+    });
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+  }
+  updateStatus = (id) => {
+    var { tasks } = this.state;
+    var index = this.findIndex(id);
+    if (index !== -1) {
+
+      tasks[index].status = !tasks[index].status
+      this.setState({
+        tasks: tasks
+      });
+      localStorage.setItem('tasks', JSON.stringify(tasks));
+    }
+  }
+  deleteItem = (id) => {
+    var { tasks } = this.state;
+    var index = this.findIndex(id);
+    if (index !== -1) {
+      tasks.splice(index, 1);
+      this.setState({
+        tasks: tasks
+      });
+      localStorage.setItem('tasks', JSON.stringify(tasks));
+    }
+    this.closeTaskForm();
+  }
+  findIndex = (id) => {
+    var { tasks } = this.state;
+    var result = -1;
+    tasks.forEach((task, index) => {
+      if (id === task.id) {
+        result = index;
+      }
+    });
+    return result;
+  }
+  editItem = (id) => {
+    var { tasks } = this.state;
+    var index = this.findIndex(id);
+    var editingTaskContent = tasks[index];
+    this.setState({
+      editingTaskContent: editingTaskContent
+    });
+    this.openTaskForm();
+  }
+  filterItems = (filterName, filterStatus) => {
+    filterStatus = parseInt(filterStatus, 10);
+    this.setState({
+      filter: {
+        name: filterName.toLowerCase(),
+        status: filterStatus
+      }
+    });
+  }
+  onSearch = (keyword) => {
+    this.setState({
+      keyword: keyword
+    });
+  }
+  onSort = (sortBy, sortValue) => {
+    this.setState({
+      sortBy: sortBy,
+      sortValue: sortValue
+    });
+  }
   render() {
+    var {
+      tasks,
+      isDisplayForm,
+      editingTaskContent,
+      filter,
+      keyword,
+      sortBy,
+      sortValue
+    } = this.state; //var tasks = this.state.tasks
+    if (filter) {
+      if (filter.name) {
+        tasks = tasks.filter((task) => {
+          return task.name.toLowerCase().indexOf(filter.name) !== -1
+        })
+      }
+      tasks = tasks.filter((task) => {
+        if (filter.status === -1) {
+          return task;
+        } else {
+          return task.status === (filter.status === 1 ? true : false)
+        }
+      });
+    }
+    if (keyword) {
+      tasks = tasks.filter((task) => {
+        return task.name.toLowerCase().indexOf(keyword) !== -1
+      })
+    }
+    if (sortBy === 'name') {
+      tasks.sort((a, b) => {
+        if(a.name > b.name) return sortValue;
+        else if (a.name < b.name) return -sortValue;
+        else return 0;
+      })
+    } else {
+      tasks.sort((a, b) => {
+        if(a.status > b.status) return -sortValue;
+        else if (a.status < b.status) return sortValue;
+        else return 0;
+      })
+    }
+    var elmTaskForm = isDisplayForm ?
+      <TaskForm
+        closeTaskForm={this.closeTaskForm}
+        onSubmit={this.onSubmit}
+        task={editingTaskContent}
+      /> : '';
     return (
       <div className="container">
         <div className="text-center">
           <h1>Quản lí công việc</h1>
         </div>
         <div className="row">
-          <div className="col-xs-4 col-sm-4 col-md-4 col-lg-4">
-            <div className="panel panel-warning">
-              <div className="panel-heading">
-                <h3 className="panel-title">
-                  Thêm công việc
-                  <span className="fa fa-times-circle text-right"
-                  ></span>
-                </h3>
-              </div>
-              <div className="panel-body">
-                <form>
-                  <div className="form-group">
-                    <label>Tên: </label>
-                    <input
-                      type="text" className="form-control" placeholder="type here"
-                      name="name"
-                      />
-                    <label>Trạng thái: </label>
-                    <select
-                      className="form-control"
-                      required="required"
-                      name="status"
-                      >
-                      <option value={0}>Kích Hoạt</option>
-                      <option value={1}>Hủy</option>
-                    </select>
-                  </div>
-                  <button type="submit" className="btn btn-warning"><i class="fa fa-plus mr-5"></i>Lưu lại</button>&nbsp;
-                  <button type="reset" className="btn btn-danger"><i class="fa fa-times mr-5"></i>Hủy bỏ</button>
-                </form>
-
-              </div>
-            </div>
+          <div className={isDisplayForm ? "col-xs-4 col-sm-4 col-md-4 col-lg-4" : ""}>
+            {elmTaskForm}
           </div>
-          <div class="col-xs-8 col-sm-8 col-md-8 col-lg-8">
-            <button type="submit" className="btn btn-primary"><i class="fa fa-plus mr-5"></i>Thêm công việc</button>
-            <div className="row">
-              <div className="col-xs-6 col-sm-6 col-md-6 col-lg-6">
-                <div>
-                  <input
-                    name="keyword"
-                    type="text"
-                    className="form-control"
-                    placeHoler="Nhập từ khóa..."
-                  />
-                  <span className="input-group-btn">
-                    <button type="submit" className="btn btn-primary"><i class="fa fa-search mr-5"></i>Tìm</button>
-                  </span>
-                </div>
-              </div>
-              <div className="col-xs-6 col-sm-6 col-md-6 col-lg-6">
-                <div className="dropdown">
-                <button type="button" className="btn btn-primary dropdown-toggle"><i class="fa fa-search mr-5"></i>Tìm</button>
-                </div>
+          <div className={isDisplayForm ? "col-xs-8 col-sm-8 col-md-8 col-lg-8" : "col-xs-12 col-sm-12 col-md-12 col-lg-12"}>
+            <button
+              type="submit"
+              className="btn btn-primary mb-5"
+              onClick={this.toggleTaskForm}
+            ><i className="fa fa-plus mr-5"></i>
+              Thêm công việc
+            </button>
+            <Control
+              onSearch={this.onSearch}
+              onSort={this.onSort}
+              sortBy={sortBy}
+              sortValue={sortValue}
+            />
+            <div className="row mt-15">
+              <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                <TaskList
+                  tasks={tasks}
+                  updateStatus={this.updateStatus}
+                  deleteItem={this.deleteItem}
+                  editItem={this.editItem}
+                  filterItems={this.filterItems}
+                />
               </div>
             </div>
-            <div className="row">
-              
-            </div>
+
           </div>
         </div>
-          
-
       </div>
-
     );
   }
 }
